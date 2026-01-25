83fff3d3680e35f68dd1be0944b2e96f4b0fd31e
Sioood <theodupontpro@gmail.com>
2026-01-25 13:45:14 +0100

    refactor(ui): :recycle: remove useless props

--- packages/ui/app/pages/index.vue
+++ packages/ui/app/pages/index.vue
@@ -1,34 +1,34 @@
 <template>
   <div class="flex flex-col">
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="red" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="red" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="red" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="red" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="red" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="red" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="blue" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="blue" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="blue" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="blue" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="blue" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="blue" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="green" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="green" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="green" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="green" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="green" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="green" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="yellow" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="yellow" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="yellow" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="yellow" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="yellow" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="yellow" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="gray" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="gray" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="gray" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="gray" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="gray" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="gray" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
     <div class="just m-6 flex items-center justify-center gap-4">
-      <UIButton color="black" size="lg" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="black" size="md" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
-      <UIButton color="black" size="sm" :has-icon="true" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="black" size="lg" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="black" size="md" icon-name="custom:plus">Primary</UIButton>
+      <UIButton color="black" size="sm" icon-name="custom:plus">Primary</UIButton>
     </div>
   </div>
 </template>
